458757af548e2b338ae56ce71797bbb8d4698d18
Riley McCarthy <rileymccarthyrileymccarthy@Rileys-MacBook-Air-3.local>
2023-04-12 11:24:53 -0400

    working build

--- builder/main.py
+++ builder/main.py
@@ -8,32 +8,9 @@ toolchain_path = env.PioPlatform().get_package_dir("toolchain-flexprop")
 flexcc = os.path.join(toolchain_path, "bin","flexcc")
 
 env.Replace(
-    AR="ar",
     AS=flexcc,
     CC=flexcc,
     CXX=flexcc,
-    OBJCOPY="objcopy",
-    RANLIB="ranlib",
-)
-
-def run_flexprop(target, source, env):
-    cmd = [
-        flexcc,
-        "-2",  # Compile for Prop2
-        "-o",
-        str(target[0]),
-        str(source[0]),
-    ]
-    env.Execute(cmd)
-
-env.Append(
-    BUILDERS=dict(
-        ElfToBin=Builder(
-            action=env.VerboseAction(f'{flexcc} -c -o $TARGET $CFLAGS $CCFLAGS $_CCCOMCOM $SOURCES', "Compiling $SOURCES"),
-            suffix=".o",
-            src_suffix=".c",
-        ),
-    )
 )
 
 if not env.get("PIOFRAMEWORK"):
