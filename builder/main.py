cb2c49a6e1c05b127eed632f94e5ecaf5aaf44c6
RileyMcCarthy <59575827+RileyMcCarthy@users.noreply.github.com>
2023-04-12 00:30:19 -0400

    Update main.py

--- builder/main.py
+++ builder/main.py
@@ -1,51 +1,33 @@
 from os.path import join
-from SCons.Script import AlwaysBuild, Builder, Default, DefaultEnvironment
+
+from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
 
-env.Replace(
-    AR="ar",
-    CC="gcc",
-    CXX="g++",
-    LINK="gcc",
-    AS="as",
-)
-
 env.Append(
-    CCFLAGS=["-Wall"],
-    LINKFLAGS=["-Wl,-Map=${TARGET}.map"],
+    BUILDERS=dict(
+        FlexProp=env.Builder(
+            action=env.VerboseAction(
+                " ".join(
+                    [
+                        join(env["PROJECT_PACKAGES_DIR"], "toolchain-flexprop", "flexcc"),
+                        "-2",
+                        "-o",
+                        "$TARGET",
+                        "$SOURCES",
+                    ]
+                ),
+                "Compiling $SOURCES",
+            ),
+            suffix=".elf",
+            src_suffix=".c",
+        ),
+    )
 )
 
-def run_flexprop(target, source, env):
-    import os
-    toolchain_path = env.PioPlatform().get_package_dir("toolchain-flexprop")
-    cmd = [
-        os.path.join(toolchain_path, "flexcc"),
-        "-2",  # Compile for Prop2
-        "-O2", # Optimization level
-        str(target[0]),
-        str(source[0]),
-    ]
-    env.Execute(cmd)
+if not env.get("PIOFRAMEWORK"):
+    env.SConscript("frameworks/_bare.py", exports="env")
 
+target_elf = env.FlexProp(join("$BUILD_DIR", "flexprop_output"), "src/main.c")
 
-env.Replace(
-    PROGNAME="flexprop_output",
-    PROGSUFFIX=".elf",
-    SIZEPROG="size",
-)
-
-env['BUILDERS']['Program'] = Builder(
-    action=env.VerboseAction(run_flexprop, "Building $TARGET"),
-    suffix=".elf",
-)
-
-# Add source files to the build environment
-env.Append(
-    CPPPATH=[join("$PROJECT_SRC_DIR")],
-    SRC_FILTER=[
-        "+<*>",
-    ],
-)
-
-Default([env.Program(join("$BUILD_DIR", "${PROGNAME}"), join("$PROJECT_SRC_DIR", "main.c"))])
+Default([target_elf])
