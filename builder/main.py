4139ddbb75c4083637343304f2f3c4f6b4cf85ba
Riley McCarthy <rileymccarthyrileymccarthy@Rileys-MacBook-Air-3.local>
2023-04-12 01:21:28 -0400

    working build file

--- builder/main.py
+++ builder/main.py
@@ -1,25 +1,36 @@
-from os.path import join
+import os
 
 from SCons.Script import DefaultEnvironment
 
 env = DefaultEnvironment()
 
+toolchain_path = env.PioPlatform().get_package_dir("toolchain-flexprop")
+flexcc = os.path.join(toolchain_path, "bin","flexcc")
+
+env.Replace(
+    AR="ar",
+    AS=flexcc,
+    CC=flexcc,
+    CXX=flexcc,
+    OBJCOPY="objcopy",
+    RANLIB="ranlib",
+)
+
+def run_flexprop(target, source, env):
+    cmd = [
+        flexcc,
+        "-2",  # Compile for Prop2
+        "-o",
+        str(target[0]),
+        str(source[0]),
+    ]
+    env.Execute(cmd)
+
 env.Append(
     BUILDERS=dict(
-        FlexProp=env.Builder(
-            action=env.VerboseAction(
-                " ".join(
-                    [
-                        join(env["PROJECT_PACKAGES_DIR"], "toolchain-flexprop", "flexcc"),
-                        "-2",
-                        "-o",
-                        "$TARGET",
-                        "$SOURCES",
-                    ]
-                ),
-                "Compiling $SOURCES",
-            ),
-            suffix=".elf",
+        ElfToBin=Builder(
+            action=env.VerboseAction(f'{flexcc} -c -o $TARGET $CFLAGS $CCFLAGS $_CCCOMCOM $SOURCES', "Compiling $SOURCES"),
+            suffix=".o",
             src_suffix=".c",
         ),
     )
@@ -28,6 +39,6 @@ env.Append(
 if not env.get("PIOFRAMEWORK"):
     env.SConscript("frameworks/_bare.py", exports="env")
 
-target_elf = env.FlexProp(join("$BUILD_DIR", "flexprop_output"), "src/main.c")
+target_elf = env.BuildProgram()
 
 Default([target_elf])
