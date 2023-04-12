8d51a59e9d44aa98aeb74adb188914092ecd1c11
RileyMcCarthy <59575827+RileyMcCarthy@users.noreply.github.com>
2023-04-11 23:40:58 -0400

    Update flexprop.py

--- builder/frameworks/flexprop.py
+++ builder/frameworks/flexprop.py
@@ -1,12 +1,11 @@
-from platformio.builder.tools import platformio as 
-platformio_builder_tools
+from platformio.builder.tools import platformio as platformio_builder_tools
 
 Import("env")
 
 def run_flexprop(target, source, env):
-    cmd = ["path/to/flexprop", "-o", str(target[0]), str(source[0])]
+    toolchain_path = env.PioPlatform().get_package_dir("toolchain-flexprop")
+    cmd = [toolchain_path + "/bin/flexprop", "-o", str(target[0]), str(source[0])]
     env.Execute(cmd)
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.elf", run_flexprop)
 env.Replace(PROGNAME="flexprop_output")
-
