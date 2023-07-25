4c6408b8b9ebcb2478451379f280cf734342b75d
Riley McCarthy <riley.mccarthy@yahoo.com>
2023-07-25 10:27:48 -0400

    upgrading multiplatform usage

--- builder/main.py
+++ builder/main.py
@@ -4,7 +4,7 @@ from SCons.Script import DefaultEnvironment
 import platform
 
 import platform
-
+from subprocess import run
 
 env = DefaultEnvironment()
 
@@ -14,14 +14,20 @@ arch = platform.machine()
 if system == "Windows":
     flexcc_executable = "flexcc.exe"
     loadp2_executable = "loadp2.exe"
+    loadp2_executable = "loadp2.exe"
 elif system == "Darwin":
     flexcc_executable = "flexcc.mac"
     loadp2_executable = "loadp2.mac"
 elif arch == "armv7l":
     flexcc_executable = "flexcc.rpi"
     loadp2_executable = "loadp2.rpi"
+    loadp2_executable = "loadp2.mac"
+elif arch == "armv7l":
+    flexcc_executable = "flexcc.rpi"
+    loadp2_executable = "loadp2.rpi"
 else:
     flexcc_executable = "flexcc"
+    loadp2_executable = "loadp2"
     loadp2_executable = "loadp2"
 
 toolchain_path = env.PioPlatform().get_package_dir("toolchain-flexprop")
@@ -37,26 +43,52 @@ env.Replace(
     CXX=flexcc, # C++ compiler
 
     UPLOADER=os.path.join(env.PioPlatform().get_package_dir("tool-loadp2"),"bin", loadp2_executable),
-    UPLOADCMD="$UPLOADER -b230400 -t $SOURCES"
+    SDCARD=os.path.join(env.PioPlatform().get_package_dir("tool-loadp2"),"bin", "loadp2-Linux", "P2ES_sdcard.bin"),
+    FLASH=os.path.join(env.PioPlatform().get_package_dir("tool-loadp2"),"bin", "loadp2-Linux", "P2ES_flashloader.bin"),
+    UPLOADERFLAGS=["-b230400"],
+    UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES",
 )
 
 # Add build flags
 env.Append(
-    CCFLAGS=["-2", "-Wall", "-O1"],
+    CCFLAGS=["-2", "-Wall", "-O1", "-D__WORKSPACE__='\"$PROJECT_DIR\"'"],
     LINKFLAGS=["-2", "-Wall", "-O1"],
     CPPPATH=[flexprop_include_path],
 )
 
+upload_actions = [env.VerboseAction("$UPLOADCMD", "Uploading $SOURCE")]
+
+upload_protocol = env.subst("$UPLOAD_PROTOCOL")
+if upload_protocol.startswith("flash"):
+    env.Replace(
+        UPLOADERFLAGS=[
+            "-t",
+            "-b230400",
+            "\"@0=$FLASH,@8000+$SOURCES\"",
+        ],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS",
+    )
+    upload_actions = ["$UPLOADCMD"]
+elif upload_protocol.startswith("sdcard"):
+    env.Replace(
+        UPLOADERFLAGS=[
+            "-t",
+            "-b230400",
+            "\"@0=$FLASH,@8000+$SOURCES\"",
+        ],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS",
+    )
+elif upload_protocol.startswith("serial"):
+    env.Replace(
+        UPLOADERFLAGS=["-t","-b230400"],
+        UPLOADCMD="$UPLOADER $UPLOADERFLAGS $SOURCES",
+    )
+    upload_actions = ["$UPLOADCMD"]
+
+
 if not env.get("PIOFRAMEWORK"):
     env.SConscript("frameworks/_bare.py", exports="env")
-
 target_elf = env.BuildProgram()
-
-#
-# Target: Upload firmware
-#
-upload = env.Alias(["upload"], target_elf, "$UPLOADCMD")
-AlwaysBuild(upload)
 
 #
 # Target: Upload firmware
